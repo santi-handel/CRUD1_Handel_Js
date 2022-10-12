fb15f17954932b94eec20197e7b3b2511b780202
Santi Handel <santiagohandel@gmail.com>
2022-10-12 11:57:11 -0300

    .

---
+++ app.js
@@ -0,0 +1,30 @@
+import { firebase } from "firebase.js";
+
+const openModal = document.getElementById("openRegisterModal");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeRegisterModal")
+const showRegisterModal = () => {
+    modal.classList.toggle("is-active");
+}
+const registerForm = document.getElementById("register-form");
+openModal.addEventListener("click", showRegisterModal);
+closeModal.addEventListener("click", showRegisterModal);
+
+const clientRef = firebase.database().ref("clientes"); 
+
+registerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nombre = registerForm["nombre"].value;
+    const apellido = registerForm["apellido"].value;
+    const domicilio = registerForm["domicilio"].value;
+    const telefono = registerForm["tel"].value;
+    const email = registerForm["email"].value;
+
+    const registrarCliente = clientRef.push()
+    console.log(registrarCliente);
+    console.log(registrarCliente.path.pieces_[1]);
+    //registarCliente.set({
+        //Uid : registarCliente.path.pieces_[1]
+    //})
+   })
